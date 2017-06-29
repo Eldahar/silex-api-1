6faa83d76596b7a030513027f2ca7096b3e7bbc9
eldahar <oravecz.tibor@edigital.hu>
2017-06-29 22:56:18 +0200

    First commit

---
+++ web/index.php
@@ -0,0 +1,13 @@
+<?php
+
+// web/index.php
+require_once __DIR__.'/../vendor/autoload.php';
+
+$app = new Silex\Application();
+
+// ... definitions
+$app-get('/api/first', function() {
+    return new \Symfony\Component\HttpFoundation\Response('First api response!');
+});
+
+$app->run();

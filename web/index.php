59e4a4e8dc2f0710a2e570cc47c44dd321037ebb
Tibor Oravecz <tibor.oravecz@gwshub.com>
2017-07-02 11:45:51 +0200

    javitas

--- web/index.php
+++ web/index.php
@@ -6,7 +6,7 @@ require_once __DIR__.'/../vendor/autoload.php';
 $app = new Silex\Application();
 
 // ... definitions
-$app-get('/api/first', function() {
+$app->get('/api/first', function() {
     return new \Symfony\Component\HttpFoundation\Response('First api response!');
 });
 
